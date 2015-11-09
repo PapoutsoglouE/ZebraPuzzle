49981041b8f3009446d0cd668603ecee571a4710
Eyaggelia Papoutsoglou <papoutsoglou.e@gmail.com>
2015-11-09 03:13:33 +0200

    First clues are placed correctly.

--- src/Household.py
+++ src/Household.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python
 
 class Household():
-	def __init__(self, position=None):
-		self.data = {"person": None, "pet": None,
-					 "drink": None, "smoke": None,
-					 "color": None}
-		self.data["position"] = position if position is not None else None				 
-		
-	
-	def describe(self):
-		pos = self.cn(self.data["position"])
-		col = self.cn(self.data["color"])
-		prs = self.cn(self.data["person"])
-		pet = self.cn(self.data["pet"])
-		drk = self.cn(self.data["drink"])
-		smk = self.cn(self.data["smoke"])
-		print("House #" + str(pos) + " is " + col + ",")
-		print("\tbelongs to a " + prs + ", ")
-		print("\thas a " + pet + " as a pet,")
-		print("\tdrinks " + drk + " and smokes " + smk + ".\n")
-	
-	def isEmpty(self):
-		if not None in self.data.values():
-			return False
-		else:
-			return True
+    def __init__(self, position=None):
+        self.data = {"person": None, "pet": None,
+                     "drink": None, "smoke": None,
+                     "color": None}
+        self.data["position"] = str(position) if str(position) is not None else None                 
+        # position is a string!
+    
+    def describe(self):
+        pos = self.cn(self.data["position"])
+        col = self.cn(self.data["color"])
+        prs = self.cn(self.data["person"])
+        pet = self.cn(self.data["pet"])
+        drk = self.cn(self.data["drink"])
+        smk = self.cn(self.data["smoke"])
+        #print("House #" + str(pos) + " is " + col + ",")
+        #print("\tbelongs to a " + prs + ", ")
+        #print("\thas a " + pet + " as a pet,")
+        #print("\tdrinks " + drk + " and smokes " + smk + ".\n")
+        print("House #" + str(pos) + ":\t" + col + ",\t" + prs + ",\t"
+               + pet + ",\t" + drk + ",\t" + smk)
+    
 
-			
-		
-	@staticmethod
-	def cn(string_to_verify):
-		if string_to_verify is None:
-			return "[?]"
-		else:
-			return string_to_verify
-			
-		
+    def isEmpty(self):
+        if not None in self.data.values():
+            return False
+        else:
+            return True
+
+            
+        
+    @staticmethod
+    def cn(string_to_verify):
+        if string_to_verify is None:
+            return "[?]"
+        else:
+            return string_to_verify
+            
+        
--- src/ZebraPuzzle.py
+++ src/ZebraPuzzle.py
@@ -17,6 +17,7 @@ def build_assertions():
     clues.append({"smoke": "old gold", "pet": "snails"})                    # 06
     clues.append({"smoke": "kools", "color": "yellow"})                     # 07
     clues.append({"drink": "milk", "position": "2"})                        # 08
+    #clues.append({"position": "2","drink": "milk"})
     clues.append({"person": "norwegian", "position": "0"})                  # 09
     clues.append({"smoke": "chesterfields", "position": "next [pet:fox]"})  # 10
     clues.append({"pet": "fox"})
@@ -43,12 +44,41 @@ def build_element_list(clues):
 
     return elements
 
+def check_houses(houses, clue):
+    """ Check how many houses clue1 <-> clue2 could describe.
+    Return number of houses, as well as the index of the last house matched. """
+    possible_placements = 0
+    possible_house = None
+
+    print("Checking houses for clue: " + str(clue))
+
+    for index, house in enumerate(houses):
+        key1 = list(clue.keys())[0]  # eg. "person"
+        key2 = list(clue.keys())[1]  # eg. "color"
+        if (house.data[key1] == clue[key1] and house.data[key2] is None):
+            # if house["person"] = "english" then house["color"] = "red"
+            house.data[key2] = clue[key2] 
+            possible_house = index
+            possible_placements += 1 
+        elif (house.data[key2] == clue[key2] and house.data[key1] is None):
+            # if house["color"] = "red" then house["person"] = "english"
+            house.data[key1] = clue[key1]
+            possible_house = index
+            possible_placements += 1 
+
+    #print("possibilities for " + str(clue) + " are " + str(possible_placements))
+    return possible_placements, possible_house
+
+
+
+
+
 def main():
     """ Solve riddle. """
     house_number = 5
     houses = list()
     for index, house in enumerate(range(house_number)):
-        houses.append(Household(str(index)))
+        houses.append(Household(index))
 
     assertions = build_assertions()
 
@@ -59,68 +89,74 @@ def main():
     pos_p = re.compile(".*\[(.*):(.*)\]")  # position pattern
     elements = build_element_list(assertions)
 
-    for clue in assertions:    
-        possible_placements = 5
-        possible_house = None
+    for clue_i, clue in enumerate(assertions):    
         if len(clue) == 2:
             if "position" not in clue.keys():
-                for index, house in enumerate(houses):
-                    key1 = list(clue.keys())[0]  # eg. "person"
-                    key2 = list(clue.keys())[1]  # eg. "color"
-                    if (house.data[key1] == clue[key1]  # if house["person"] = "english"
-                        and house.data[key1] is None):
-                        house.data[key2] = clue[key2]   # then house["color"] = "red"
-                        possible_house = index
-                    elif (house.data[key2] == clue[key2]  # if house["color"] = "red"
-                        and house.data[key1] is None):
-                        house.data[key1] = clue[key1]     # then house["person"] = "english"
-                        possible_house = index
-                    else:  # neither clue matches 
-                        possible_placements -= 1
-                    print("possibilities for " + str(clue) + " are " + str(possible_placements))
-
-                if possible_placements == 1:
-                    if (houses[possible_placements].data[key1] == clue[key1] and 
-                        houses[possible_placements].data[key2] is None):
+                possible_placements, possible_house = check_houses(houses, clue)
+                if possible_placements == 1:  # guaranteed fit, one way or another
+                    assertion_used[clue_i] = True
+                    if houses[possible_house].data[key1] == clue[key1]:
                          # if house["person"] = "english" then house["color"] = "red"
-                        houses[possible_placements].data[key2] = clue[key2]
-                    elif (houses[possible_placements].data[key2] == clue[key2] and  
-                        houses[possible_placements].data[key1] is None):
+                        houses[possible_house].data[key2] = clue[key2]
+                    else:  # houses[possible_house].data[key2] == clue[key2]
                         # if house["color"] = "red" then house["person"] = "english"
-                        houses[possible_placements].data[key1] = clue[key1]
-            else:  # position in clue
-                position = clue["position"]
-                # get other key
-                keys = list(clue.keys())
-                keys.remove("position")
-                key2 = keys[0]
-                print("key2 is: " + key2 + ", position: " + position)
-                if "right" in position:  
-                    print("placeholder: position with right")
-                    # clue 05: {"color": "green", "position": "right [color:ivory]"}
-                    res = re.match(pos_p, position)
-                    comp_key = res.group(1)  # comparison key: color
-                    comp_val = res.group(2)  # comparison value: ivory
-                    for i, h in enumerate(houses):
-                        if h.data[comp_key] == comp_val:
-                            pass
+                        houses[possible_house].data[key1] = clue[key1]
 
-                elif "left" in position:
-                    print("placeholder: position with left")
-                elif "next" in position:
-                    print("placeholder: position with next")
-                    # clue 11: {"smoke": "kools", "position": "next [pet:horse]"}
-                elif position.isdigit():
-                    print("placeholder: position with digit")
-                else:
-                    print("Clue contains invalid position: " + position + ". Exiting...")
-                    exit()
+            else:  # position in clue
+                position = clue["position"] # key1 is "position"
+                key2 = list(clue.keys())  # get other key
+                key2.remove("position")  
+                key2 = key2[0]
+                print("\n\nkey2 is: " + key2 + ", position: " + position)
+                pos_data = re.match(pos_p, position)  # group(1) = "color", group(2) = "ivory"
+
+                if pos_data is None:  # no pattern match
+                    if position.isdigit() and int(position) < house_number:
+                        possible_placements, possible_house = check_houses(houses, clue)
+                        print("poss_pl: " + str(possible_placements) + ", poss_h: " + str(possible_house))
+                        print("for clue: " + str(clue))
+                        if possible_placements == 1:  # guaranteed fit, one way or another
+                            assertion_used[clue_i] = True
+                            if houses[possible_house].data["position"] == clue["position"]:
+                                 # if house["person"] = "english" then house["color"] = "red"
+                                houses[possible_house].data[key2] = clue[key2]
+                            else:  # houses[possible_house].data[key2] == clue[key2]
+                                # if house["color"] = "red" then house["person"] = "english"
+                                houses[possible_house].data["position"] = clue["position"]
+
+                    else:
+                        print("Clue contains invalid position: " + position + ". Exiting...")
+                        exit()
+
+                else:  # pattern matched
+                    comp_key = pos_data.group(1)  # comparison key: color
+                    comp_val = pos_data.group(2)  # comparison value: ivory
+
+                    if "right" in position:  
+                        print("placeholder: position with right")
+                        # clue 05: {"color": "green", "position": "right [color:ivory]"}
+
+                        for i, h in enumerate(houses):
+                            if h.data[comp_key] == comp_val:  # if color = ivory
+                                pass
+
+                    elif "left" in position:
+                        print("placeholder: position with left")
+                    elif "next" in position:
+                        print("placeholder: position with next")
+                        # clue 11: {"smoke": "kools", "position": "next [pet:horse]"}
+                    else:
+                        print("Clue contains invalid position modifier: " + position + ". Exiting...")
+                        exit()
+
                     
                     
     #for e in elements:
         #print(e + ":    \t" + str(elements[e]) + "\n")
+    print("House ##,\tcolor,\tresident,\tpet,\tdrink,\tsmoke")
     for house in houses:
         house.describe()
+    print("Assertions used:\n" + str(assertion_used))
 
 
 
